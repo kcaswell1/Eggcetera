b645d7c1abd0dae5f7cf9801cceea8f2e3869c26
kcaswell1 <50675382+kcaswell1@users.noreply.github.com>
2019-06-20 17:29:00 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-$(".btn").click(function() {
-    $(".btn").css(
-        {'background-color': 'yellow',
-        'color': 'black'});
+$(function() {
+    $(".btn").click(function() {
+        $(".btn").css(
+            {'background-color': 'yellow',
+            'color': 'black'});
+    });
 });
